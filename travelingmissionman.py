cff1f566e2d291fbbf0c5ad3eb3112a542fa8fb6
Higgs Boson <no@no.org>
2020-08-28 10:02:19 -0500

    Prettier output, input checking

--- travelingmissionman.py
+++ travelingmissionman.py
@@ -1,25 +1,18 @@
 import json, itertools
 from datetime import datetime
 
+
 def calculate_distance(systems, distances):
     dist = 0
-    for i in range(len(systems)-1):
+    for i in range(len(systems) - 1):
         first_system = systems[i]
-        second_system = systems[i+1]
+        second_system = systems[i + 1]
         distances[first_system][second_system]
-        dist += distances[systems[i]][systems[i+1]]
+        dist += distances[systems[i]][systems[i + 1]]
     return dist
 
+
 def main():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Begin at", current_time)
-    with open("output.json") as input:
-        distances = json.load(input)
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Finished loading json at", current_time)
-    print(distances['30000142'])
     names = {}
     system_ids = {}
     with open("systems.csv") as system_names:
@@ -32,27 +25,52 @@ def main():
             system_ids[system_name] = system_id
 
     mission_systems = []
+    found_systems = True
     with open('missions.txt') as input_file:
         for line in input_file:
             system_name = line.strip()
-            system_id = system_ids[system_name]
+            try:
+                system_id = system_ids[system_name]
+            except KeyError:
+                found_systems = False
+                print("Unable to find system", system_name)
             mission_systems.append(system_id)
+    start_system = [mission_systems.pop(0)]
+    if found_systems is False:
+        print("Could not find all the systems in missions.txt, please check and retry.")
+        exit()
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Begin at", current_time)
+    with open("output.json") as input:
+        distances = json.load(input)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Finished loading json at", current_time)
 
     min_path_distance = 1000000000
 
     for permutation in itertools.permutations(mission_systems):
-        distance = calculate_distance(permutation, distances)
+        total_path = tuple(start_system) + permutation
+        distance = calculate_distance(total_path, distances)
         if distance < min_path_distance:
-            shortest_path = permutation
+            shortest_path = total_path
             min_path_distance = distance
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    print("Found shortest_path at",current_time)
+    print("Found shortest_path at", current_time)
     short_systems = []
-    for item in shortest_path:
-        short_systems.append(names[item])
-    print(short_systems, min_path_distance)
-
+    for i in range(len(shortest_path) - 1):
+        first_system_id = shortest_path[i]
+        second_system_id = shortest_path[i + 1]
+        first_system_name = names[first_system_id]
+        second_system_name = names[second_system_id]
+        jump_distance = distances[first_system_id][second_system_id]
+        print(first_system_name, "->", jump_distance, "jumps ->", second_system_name)
+        # print(names)
+        # short_systems.append(names[item])
+    print("Total distance",min_path_distance,"jumps.")
+    # print(short_systems, min_path_distance)
 
 
 if __name__ == "__main__":
